4f57940f266344b8d32e30f0a8ee401d2f2338e4
sunnysunshine <40585336+andohong@users.noreply.github.com>
2023-07-24 15:27:53 +0700

    Add files via upload

--- users.js
+++ users.js
@@ -1,18 +1,32 @@
-const express = require ('express');
-const app = express ();
-const xml = require ('xml')
-app.get('/users',(req,res,next)=> {
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>`
-    xml += `<user>`
-    for (let i = 0; i<99; i++){
-        xml += `<customer> 
-        <firstName>Henry</firstName>
-        <lastName>William</lastName>
-    </customer>`
-    }
-    xml += `</user>`
-    res.header('Content-Type', 'application/xml')
-    res.status(200).send(xml)
+//import fs from "fs";
+//import { parseString } from "xml2js";
 
+//const fs = require('fs');
+//
+//console.log(xml_string);
+
+
+//const xml2js = require('xml2js');
+//var xmldata = "user.xml";
+//let xml_string = fs.readFileSync("user.xml", "utf8");
+
+//xml2js.parseString(xml_string, function (err, results) {
+//  let data = JSON.stringify(results)
+//  console.log("resuts", data)
+//})
+
+import fs from "fs";
+import xml2js from "xml2js";
+fs.readFile("users.xml", "utf8", function (err, data) {
+    let json
+    if (err) {
+        console.error(err);
+        return;
+    }
+    let parser = new xml2js.Parser();
+    parser.parseString(data.substring(0, data.length), function (err, result) {
+        json = JSON.stringify(result);
+        console.log(JSON.stringify(result));
+    });
+    return json;
 })
-app.listen(5000, (res)=>{console.log ('server started on port :5000')})
