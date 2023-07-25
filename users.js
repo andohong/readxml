4b2f5aa728235fbd682737bafdf859b21b12ccb3
sunnysunshine <40585336+andohong@users.noreply.github.com>
2023-07-25 17:00:44 +0700

    Add files via upload

--- users.js
+++ users.js
@@ -1,24 +1,12 @@
-//import fs from "fs";
-//import { parseString } from "xml2js";
-
-//const fs = require('fs');
-//
-//console.log(xml_string);
-
-
-//const xml2js = require('xml2js');
-//var xmldata = "user.xml";
-//let xml_string = fs.readFileSync("user.xml", "utf8");
-
-//xml2js.parseString(xml_string, function (err, results) {
-//  let data = JSON.stringify(results)
-//  console.log("resuts", data)
-//})
 
 import fs from "fs";
 import xml2js from "xml2js";
-fs.readFile("users.xml", "utf8", function (err, data) {
-    let json
+import express from "express";
+
+const app = express();
+
+let json = fs.readFile("user.xml", "utf8", function (err, data) {
+    //let json
     if (err) {
         console.error(err);
         return;
@@ -26,7 +14,16 @@ fs.readFile("users.xml", "utf8", function (err, data) {
     let parser = new xml2js.Parser();
     parser.parseString(data.substring(0, data.length), function (err, result) {
         json = JSON.stringify(result);
-        console.log(JSON.stringify(result));
+        //console.log(JSON.stringify(result));
+        console.log(json);
+
     });
     return json;
+
+})
+app.get('/', function (req, res) {
+    res.send(json);
+})
+app.listen(5001, (res) => {
+    console.log('Server started on port : 5001')
 })
